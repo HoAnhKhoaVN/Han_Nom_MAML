62f314bfde699af82f5978207be23197f9fda1ce
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-08-17 21:22:12 +0700

    Add han nom dataset

--- run_han_nom.sh
+++
@@ -1,4 +0,0 @@
-python han_nom_maml_train.py --epoch 1 \
-                             --root '../../Dataset/demo_ds' \
-                             --imgsz 32 \
-                             --task_num 1 \
